eb999484f30b3ba8a0e6db716207468e6a487463
邓江洲 <707703386@qq.com>
2020-08-10 14:53:44 +0800

    669. Trim a Binary Search Tree

--- restart/restart/Tree/D.swift
+++ restart/restart/Tree/D.swift
@@ -11,11 +11,28 @@ import Foundation
 
 
 class Solution {
+    
+    
+    // in order, 改一下 中序遍历
+    
     func trimBST(_ root: TreeNode?, _ L: Int, _ R: Int) -> TreeNode? {
-            
         
-        
-        
-        return root
+        guard let n = root else {
+            return nil
+        }
+        switch n.val {
+        case ..<L:
+            // 左边结点，都没用了
+            return trimBST(root?.right, L, R)
+        case L...R:
+            n.left = trimBST(root?.left, L, R)
+            n.right = trimBST(root?.right, L, R)
+            return n
+        case R...:
+            // 右边结点，都没用了
+            return trimBST(root?.left, L, R)
+        default:
+            return nil
+        }
     }
 }
--- restart/restart/main.swift
+++ restart/restart/main.swift
@@ -10,3 +10,16 @@ import Foundation
 
 
 
+let s = Solution()
+
+var src = [1, 0, 2]
+var lhs = 1
+var rhs = 2
+src = [3, 0, 4, 0, 2, 0, 0, 0,  0,  1]
+rhs = 3
+let node = src.toTree
+
+let r = s.trimBST(node, lhs, rhs)
+
+
+print(r)
