e2dcb913ab017d919ff7c31a33febbf164ccbeba
邓江洲 <707703386@qq.com>
2020-08-14 10:37:04 +0800

    prepared always

--- restart/restart/Tree/L.swift
+++ restart/restart/Tree/L.swift
@@ -16,9 +16,23 @@ class Solution {
     
     
     func searchBST(_ root: TreeNode?, _ val: Int) -> TreeNode? {
- 
-        
-        
+        guard let n = root else {
+            return nil
+        }
+        var queue = [TreeNode]()
+        queue.append(n)
+        while queue.isEmpty == false {
+            let piece = queue.removeFirst()
+            if piece.val == val{
+                return piece
+            }
+            if let part = piece.left{
+                queue.append(part)
+            }
+            if let part = piece.right{
+                queue.append(part)
+            }
+        }
         return nil
     }
 }
