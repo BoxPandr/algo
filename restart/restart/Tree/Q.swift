5ddb94fc1849c2c2bfd1cb00db9ee2c4bf75aa9a
邓江洲 <707703386@qq.com>
2020-08-24 17:35:41 +0800

    606. Construct String from Binary Tree

--- restart/restart/Tree/Q.swift
+++ restart/restart/Tree/Q.swift
@@ -11,7 +11,7 @@ import Foundation
 
 //  617
 
-class Solution {
+class Solution_Q {
     func mergeTrees(_ t1: TreeNode?, _ t2: TreeNode?) -> TreeNode? {
         guard let left = t1 else{
             return t2
@@ -28,10 +28,13 @@ class Solution {
                 if let otherL = n.1.left{
                     queue.append((lhs, otherL))
                     n.0.left?.val = lhs.val + otherL.val
+                    // 两边都有，就继续拓展
                 }
+                // main 是 left, main 存在，另一边不存在，不用管
             }
             else{
                 n.0.left = n.1.left
+                // main 是 left, main 不存在，直接采用另一边
             }
             if let rhs = n.0.right{
                 if let otherR = n.1.right{
---
+++ restart/restart/Tree/R.swift
@@ -0,0 +1,22 @@
+//
+//  R.swift
+//  restart
+//
+//  Created by Jz D on 2020/8/24.
+//  Copyright © 2020 Jz D. All rights reserved.
+//
+
+import Foundation
+
+
+// 606. Construct String from Binary Tree
+
+
+
+class Solution {
+    func tree2str(_ t: TreeNode?) -> String {
+ 
+        
+        return ""
+    }
+}
