564c099ff0aea31ace42b5e6490fdc0bb842074d
邓江洲 <707703386@qq.com>
2020-08-31 17:39:42 +0800

    func isSymmetric(_ root: TreeNode?) -> Bool

--- restart/restart/Tree/T.swift
+++ restart/restart/Tree/T.swift
@@ -12,10 +12,47 @@ import Foundation
 
 class Solution {
     func isSymmetric(_ root: TreeNode?) -> Bool {
- 
+        guard let n = root else{
+            return true
+        }
+        var queue = [n]
+        var ok = true
+        while queue.isEmpty == false {
+            var result = [Int]()
+            var level = [TreeNode]()
+            for ele in queue{
+                if let lhs = ele.left{
+                    level.append(lhs)
+                    result.append(lhs.val)
+                }
+                else{
+                    result.append(-1)
+                }
+                if let rhs = ele.right{
+                    level.append(rhs)
+                    result.append(rhs.val)
+                }
+                else{
+                    result.append(-1)
+                }
+            }
+            var i = 0
+            var j = result.count - i - 1
+            while i < j {
+                if result[i] != result[j]{
+                    ok = false
+                    return ok
+                }
+                i += 1
+                j = result.count - i - 1
+            }
+            if ok == false{
+                return ok
+            }
+            queue = level
+            
+        }
         
-        
-        
-        return true
+        return ok
     }
 }
--- restart/restart/main.swift
+++ restart/restart/main.swift
@@ -11,22 +11,18 @@ import Foundation
 
 
 
-//let s = Solution_S()
-//
-//var src: [Int?] = [1, nil ,3,2,4, nil,5,6]
-//
-//src = [1,nil,2,3,4,5,nil,nil,
-//       6,7,nil,8,nil,9,10,nil,
-//       nil,11,nil,12,nil,13,nil,nil,
-//       14]
-//
-//let node = src.treeN
-//
-//
-//var result = s.postorder(node)
-//
-//
-//print(result)
+let s = Solution()
+
+var src: [Int?] = [1,2,2,3,4,4,3]
+
+src = [1,2,2,nil,3,nil,3]
+
+let node = src.toTree
+
+
+var result = s.isSymmetric(node)
+
+print(result)
 
 
 
