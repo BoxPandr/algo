e2261849561254817299ae015062dd351cdef687
邓江洲 <707703386@qq.com>
2020-08-31 16:37:59 +0800

    // 多叉树     var treeN: Node?{

--- restart/restart/Tree/Tool/N_Array.swift
+++ restart/restart/Tree/Tool/N_Array.swift
@@ -27,24 +27,37 @@ public class Node {
 
 
 extension Array where Element == Int?{
-    
+    // 多叉树
     var treeN: Node?{
         guard isEmpty == false, let first = self[0] else {
             return nil
         }
         
         let root = Node(first)
+        
+        //  对应 ， j
         var queue = [root]
+        // 处理父结点
+        
+        
+        var q = [root.val]
+        
+        // 跳过 root, 和
+        // root 之后的第一个 nil
         var i = 2
+        
+        
         var j = 0
         while i < count{
             var children = [Node]()
-            while self[i] != nil{
+            while i < count, self[i] != nil{
                 children.append(Node(self[i]!))
                 i += 1
             }
+            i += 1
             queue[j].children = children
             queue.append(contentsOf: children)
+            q.append(contentsOf: children.map({ $0.val }))
             j += 1
         }
         
