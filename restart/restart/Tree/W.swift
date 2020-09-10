171e1b27516c27cea6900f9bb9e1d12b2623a498
邓江洲 <707703386@qq.com>
2020-09-10 17:48:07 +0800

    //  559. Maximum Depth of N-ary Tree

--- restart/restart/Tree/W.swift
+++ restart/restart/Tree/W.swift
@@ -25,4 +25,27 @@ class Solution_W{
         
         return result
     }
+    
+    
+    
+    func test(){
+        let solve = Solution_W()
+
+
+        var src = [1, nil ,3,2,4, nil ,5,6]
+
+        src = [1,nil,2,3,4,
+               5,nil,nil,6,7,
+               nil,8,nil,9,10,
+               nil,nil,11,nil,12,
+               nil,13,nil,nil,14]
+
+        let node = src.treeN
+
+
+        let result = solve.preorder(node)
+
+
+        print(result)
+    }
 }
--- restart/restart/Tree/X.swift
+++ restart/restart/Tree/X.swift
@@ -7,3 +7,54 @@
 //
 
 import Foundation
+
+
+
+//  559. Maximum Depth of N-ary Tree
+
+class Solution_X {
+    func maxDepth(_ root: Node?) -> Int {
+        
+        guard let n = root else {
+            return 0
+        }
+        
+        var result = 1
+        for node in n.children{
+            
+            result = max(result, maxDepth(node) + 1)
+            
+            
+        }
+        
+        
+        return result
+    }
+    
+    
+    
+    func test(){
+
+        let solve = Solution_X()
+
+
+        var src = [1, nil ,3,2,4, nil ,5,6]
+         
+         
+        src = [1,nil,2,3,4,
+               5,nil,nil,6,7,
+               nil,8,nil,9,10,
+               nil,nil,11,nil,12,
+               nil,13,nil,nil,14]
+
+
+
+        let node = src.treeN
+
+
+        let result = solve.maxDepth(node)
+
+
+        print(result)
+    }
+}
