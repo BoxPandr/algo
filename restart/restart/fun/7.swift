2787bcf19d889bd0f0878e05921c7b5362538d07
邓江洲 <707703386@qq.com>
2020-09-10 17:00:36 +0800

    40. 组合总和 II

--- restart/restart/fun/7.swift
+++ restart/restart/fun/7.swift
@@ -7,3 +7,85 @@
 //
 
 import Foundation
+
+
+//  40. 组合总和 II
+
+
+class Solution__7{
+    func combinationSum2(_ candidates: [Int], _ target: Int) -> [[Int]] {
+        
+        guard candidates.count > 1, target > 0 else {
+                if candidates.count == 1, target - candidates[0] == 0{
+                    return [candidates]
+                }
+                else{
+                    return []
+                }
+                
+            }
+        
+        
+        
+            var met = false
+            for piece in candidates{
+                if piece <= target{
+                    met = true
+                }
+            }
+            guard met else {
+                return []
+            }
+            // 前面是终止条件
+            
+            
+            
+            
+            
+            var possible = candidates.sorted()
+        //    print(possible)
+        //    print(target)
+        //    print("\n")
+            var results = [[Int]]()
+            while possible.isEmpty == false {
+                let last = possible.removeLast()
+                
+                
+                    switch target ¥ last {
+                    case .small:
+                        ()
+                    case .equal:
+                        results.append([last])
+                    case .big:
+                        // 包含
+                        var contain = combinationSum2(possible, target - last)
+                   //     print("contain")
+                  //      print(contain)
+                        if contain.isEmpty == false{
+                            
+                            var j = 0
+                            while j < contain.count {
+                                contain[j].append(contentsOf: [last])
+                                j += 1
+                            }
+                            results.append(contentsOf: contain)
+                        }
+                    }
+                    
+              
+                
+
+            }
+            
+            
+           // results = Array(Set(results))
+            
+            
+            return results
+        
+        
+        
+        
+        
+    }
+}
