50dda2a88944fbab5923c33c6aaf94756254bc0b
邓江洲 <707703386@qq.com>
2020-09-13 12:10:40 +0800

    // 建表， 查表
        var map = [[Int]]()

--- restart/restart/contest/_h.swift
+++ restart/restart/contest/_h.swift
@@ -15,10 +15,55 @@ import Foundation
 
 
 class Solution___h {
+    // 建表， 查表
     
+    
+    var map = [[Int]]()
     
     // 不是依次连接
     func minCostConnectPoints(_ points: [[Int]]) -> Int {
+        
+        
+        let rowCount = points.count
+        guard rowCount > 1 else {
+            return 0
+        }
+        var i = 0
+        while i < rowCount {
+            var j = 0
+            
+            while j < rowCount {
+                if i == j{
+                    map[i].append(-1)
+                }
+                else{
+                    let val = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1])
+                    map[i].append(val)
+                }
+                
+                j += 1
+            }
+            
+            
+            i += 1
+        }
+        
+        var result = 0
+        
+        
+        
+        
+        
+        return result
+        
+    }
+    
+    
+    
+    
+    
+    // 不是依次连接
+    func minCostConnectPoints_time_out(_ points: [[Int]]) -> Int {
         let count = points.count
         guard count > 1 else {
             return 0
